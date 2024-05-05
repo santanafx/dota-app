3d411910e849f8be7927cc8fc6dab7a46e8adaa1
Lucas Santana <santanafx@hotmail.com>
2024-05-05 16:11:55 -0300

    feat: added all components to Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
+import Header from "../components/Header/Header";
+import HomeMenu from "../components/HomeMenu/HomeMenu";
+import Logo from "../components/Logo/Logo";
+import style from "./Home.module.css";
 
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={style.homeContainer}>
+      <Header>
+        <Logo />
+        <HomeMenu>
+          <HomeMenu.OptionContainer>
+            <HomeMenu.SearchInput />
+            <HomeMenu.Option text={"Home"} />
+            <HomeMenu.Option text={"Meta"} />
+            <HomeMenu.Option text={"Partidas"} />
+            <HomeMenu.Option text={"Jogadores"} />
+          </HomeMenu.OptionContainer>
+        </HomeMenu>
+      </Header>
+    </div>
   )
 }
